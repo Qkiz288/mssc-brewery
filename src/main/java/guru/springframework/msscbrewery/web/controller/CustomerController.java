443833195f6e8d6f8f115a66cbb009e99a2a8938
Kamil <qkiz288@gmail.com>
2020-07-09 13:42:52 +0200

    added MvcExceptionHandler

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -5,11 +5,7 @@ import guru.springframework.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("api/v1/customer")
@@ -43,15 +39,5 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCustomer(@PathVariable UUID customerId) {
         customerService.deleteCustomer(customerId);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
